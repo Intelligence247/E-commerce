49a25c451bee90ac5de9085162c26fed956777a7
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-22 12:04:17 +0100

    fixes

---
+++ src/Component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+              <p>+000123342</p>
+              <p>Get 50% Off on Selected Items</p>
+              <p className='lang'>
+                <p>Eng </p>
+                <p>Location</p>
+              </p>
+            </header>
+  )
+}
