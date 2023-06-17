cf97535f288f81c316dc07d43778377e85ca4ae7
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-06-17 23:04:50 +0100

    starting afresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Header } from './Component/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-           <div className="wrapper">
-            <Header/>
-           </div>
+           
     </div>
   )
 }
