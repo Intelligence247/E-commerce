62fd9fbd4b2a8a9bdb8343acd310fc08de0c22a3
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-07-08 11:30:36 +0100

    fixes

---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="heroW">
+      <div>
+        <div className="heroContent">
+          <h1>Welcome to</h1>
+          <p>Abdul's Car</p>
+          <button>Explore</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -3,9 +3,11 @@ import EachProduct from '../../Component/EachProduct';
 import { PRODUCTS } from '../../Products';
 import './Shop.css';
 import About from '../../Component/About/About';
+import Hero from '../../Component/Hero/Hero';
 const Shop = () => {
   return (
     <div className="ShopW">
+      <Hero />
       <About />
       <h1>Welcome to my Shop</h1>
       <div className="products">
