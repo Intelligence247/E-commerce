aaeaecde11382895eb5223c9a21f9fe7d1ee7d1a
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-07-11 00:51:49 +0100

    fixes

--- src/Component/Search/Search.jsx
+++ src/Component/Search/Search.jsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import './Search.css';
-import { useState } from 'react';
 
-const Search = ({
-  handleAllclick,
-  handleRolceClick,
-  handleLexusClick,
-  setIndexed,
-}) => {
+const Search = ({ setIndexed, setInputs }) => {
   const btnArr = ['ALL', 'BMW', 'BENZ', 'ROLCE ROYS', 'LEXUS'];
   return (
     <div className="searchW">
       <div className="btns">
-        {/* <button onClick={handleAllclick}>All</button>
-        <button onClick={handleBmwClick}>BMW</button>
-        <button onClick={handleBenzClick}>BENZ</button>
-        <button onClick={handleRolceClick}>ROLCE ROYS</button>
-        <button onClick={handleLexusClick}>LEXUS</button> */}
         {btnArr.map((b, i) => (
-          // <p></p>
           <button key={i} onClick={() => setIndexed(i)}>
             {b}
           </button>
         ))}
       </div>
       <div className="inputs">
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => setInputs(e.target.value.toUpperCase())}
+        />
       </div>
     </div>
   );
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -5,29 +5,15 @@ import './Shop.css';
 import About from '../../Component/About/About';
 import Hero from '../../Component/Hero/Hero';
 import Search from '../../Component/Search/Search';
+
 const Shop = () => {
-  // const [BMW, setBMW] = useState({});
-  const [PRODUCT, setPRODUCT] = useState([]);
   const [Indexed, setIndexed] = useState(0);
-  // const what = Indexed===0?n.productName: Indexed===1?"BMW": Indexed===2?'BENZ':Indexed===3?"ROLCE ROYS":Indexed===4?'LEXUS':n.productName
-  console.log(Indexed);
-  const filtered = PRODUCTS.filter((n) =>
-    (n.productName === Indexed) === 0
-      ? n.productName
-      : Indexed === 1
-      ? 'BMW'
-      : Indexed === 2
-      ? 'BENZ'
-      : Indexed === 3
-      ? 'ROLCE ROYS'
-      : Indexed === 4
-      ? 'LEXUS'
-      : n.productName,
-  );
+  const [Inputs, setInputs] = useState('');
 
   const filtereds = PRODUCTS.filter((n) => {
-    // return n.productName === 'ROLCE ROYS';
-    if (Indexed === 0) {
+    if (n.productName.includes(Inputs)) {
+      return n.productName === Inputs;
+    } else if (Indexed === 0) {
       return n.productName === n.productName;
     } else if (Indexed === 1) {
       return n.productName === 'BMW';
@@ -38,10 +24,11 @@ const Shop = () => {
     } else if (Indexed === 4) {
       return n.productName === 'LEXUS';
     } else {
-      return n.productName === n.productName;
+      return '';
     }
   });
-  console.log(filtereds);
+  console.log(Inputs);
+  const mapping = PRODUCTS.map((p) => {});
   return (
     <div className="ShopW">
       <Hero />
@@ -49,7 +36,7 @@ const Shop = () => {
       <h1 id="store">
         Our <span>Store</span>
       </h1>
-      <Search setIndexed={setIndexed} />
+      <Search setIndexed={setIndexed} setInputs={setInputs} />
       <div className="products">
         {filtereds.map((product, i) => (
           <div key={i}>
